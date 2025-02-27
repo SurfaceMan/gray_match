fa61990d3c9c76ec2c868ab1efac3e87218eb519
y.qiu <y.qiu@local>
2025-02-27 20:08:44 +0800

    add default image

--- main.cpp
+++ main.cpp
@@ -4,10 +4,8 @@
 #include <opencv2/opencv.hpp>
 
 int main() {
-    auto src =
-        cv::imread("C:/Users/qiuyong/Desktop/test/template/model3.bmp", cv::IMREAD_GRAYSCALE);
-    auto dst =
-        cv::imread("C:/Users/qiuyong/Desktop/test/template/model3_src2.bmp", cv::IMREAD_GRAYSCALE);
+    auto src = cv::imread(std::string(IMG_DIR) + "/model3.png", cv::IMREAD_GRAYSCALE);
+    auto dst = cv::imread(std::string(IMG_DIR) + "/model3_src2.png", cv::IMREAD_GRAYSCALE);
     if (src.empty() || dst.empty()) {
         return -1;
     }
@@ -22,25 +20,25 @@ int main() {
     const auto matchCost = static_cast<double>(t2 - t1) / cv::getTickFrequency();
     std::cout << "train(s):" << trainCost << " match(s):" << matchCost << std::endl;
 
-    // cv::Mat color;
-    // cv::cvtColor(dst, color, cv::COLOR_GRAY2RGB);
+    cv::Mat color;
+    cv::cvtColor(dst, color, cv::COLOR_GRAY2RGB);
     for (const auto &pose : poses) {
-        // cv::RotatedRect rect(cv::Point2f(pose.x, pose.y), src.size(), -pose.angle);
-        //
-        // cv::Point2f pts[ 4 ];
-        // rect.points(pts);
-        //
-        // cv::line(color, pts[ 0 ], pts[ 1 ], cv::Scalar(255, 0, 0), 1, cv::LINE_AA);
-        // cv::line(color, pts[ 1 ], pts[ 2 ], cv::Scalar(255, 0, 0), 1, cv::LINE_AA);
-        // cv::line(color, pts[ 2 ], pts[ 3 ], cv::Scalar(255, 0, 0), 1, cv::LINE_AA);
-        // cv::line(color, pts[ 3 ], pts[ 0 ], cv::Scalar(255, 0, 0), 1, cv::LINE_AA);
+        cv::RotatedRect rect(cv::Point2f(pose.x, pose.y), src.size(), -pose.angle);
+
+        cv::Point2f pts[ 4 ];
+        rect.points(pts);
+
+        cv::line(color, pts[ 0 ], pts[ 1 ], cv::Scalar(255, 0, 0), 1, cv::LINE_AA);
+        cv::line(color, pts[ 1 ], pts[ 2 ], cv::Scalar(255, 0, 0), 1, cv::LINE_AA);
+        cv::line(color, pts[ 2 ], pts[ 3 ], cv::Scalar(255, 0, 0), 1, cv::LINE_AA);
+        cv::line(color, pts[ 3 ], pts[ 0 ], cv::Scalar(255, 0, 0), 1, cv::LINE_AA);
 
         std::cout << pose.x << "," << pose.y << "," << pose.angle << "," << pose.score << //
             std::endl;
     }
-    //
-    // cv::imshow("img", color);
-    // cv::waitKey();
+
+    cv::imshow("img", color);
+    cv::waitKey();
 
     return 0;
 }
